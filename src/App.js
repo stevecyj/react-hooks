3ca208f13bde46ff9ab3037cf1798bb41e70de46
Steve <ycsteven.tsao@gmail.com>
2023-01-12 00:31:10 +0800

    useState

--- src/App.js
+++ src/App.js
@@ -3,9 +3,24 @@ import './App.css';
 
 function App() {
   const [ count, setCount ] = useState(0);
+  const [ flag, setFlag ] = useState(true);
+  const [ list, setList ] = useState([]);
+  console.log(count);
+
+  function test() {
+    setCount(count + 1);
+    setFlag(false);
+    setList([ 1, 2, 3 ]);
+  }
+
   return (
     <div className="App">
-      <button onClick={() => setCount(count + 1)}>{count}</button>
+      count:{count}
+      <hr/>
+      flag:{flag ? 'true' : 'false'}
+      <hr/>
+      list:{list.join('-')}
+      <button onClick={test}>+</button>
     </div>
   );
 }
