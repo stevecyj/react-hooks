068593a15803a3f6192eeed03f7b541f606b5cca
Steve <ycsteven.tsao@gmail.com>
2023-01-12 08:03:38 +0800

    useEffect,depends

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 
 function App() {
   const [ count, setCount ] = useState(0);
+  const [ name, setName ] = useState('zs');
   const [ flag, setFlag ] = useState(true);
   const [ list, setList ] = useState([]);
   console.log(count);
 
   useEffect(() => {
+    console.log('副作用執行了');
     document.title = count;
-  });
+    console.log(name);
+  }, [ count, name ]);
 
   function test() {
     setCount(count + 1);
@@ -25,6 +28,10 @@ function App() {
       <hr/>
       list:{list.join('-')}
       <button onClick={test}>+</button>
+      <br/>
+      <button onClick={() => {
+        setName('cp');
+      }}>{name}</button>
     </div>
   );
 }
