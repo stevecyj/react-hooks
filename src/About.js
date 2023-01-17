29cfac2a1cb5650077a688a74971c9f3ae29ee01
Steve <ycsteven.tsao@gmail.com>
2023-01-17 20:20:51 +0800

    react-router: useParams

--- src/About.js
+++ src/About.js
@@ -1,10 +1,14 @@
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams, useParams } from "react-router-dom";
 
 function About() {
-  const [ params ] = useSearchParams();
-  const id = params.get('id');
-  const name = params.get('name');
-  return (<div>About,得到的參數 id：{id},name：{name}</div>);
+  // const [ params ] = useSearchParams();
+  const params = useParams();
+
+  // const id = params.get('id');
+  // const name = params.get('name');
+
+  const { id } = params;
+  return (<div>About,得到的參數, id：{id}</div>);
 }
 
 export default About;
